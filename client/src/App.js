cdebd0f53b6227d09fbc9479587690b8d2918f00
Erol Bakic <erol.bakic@hotmail.com>
2020-05-17 13:00:41 +0200

    • Som bruger af systemet, skal det være mulighed for at dele sine billeder med udvalgte venner i systemet.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LoginPage from './Components/loginpage/loginpage.component.jsx';
 import RegisterPage from './Components/registerpage/registerpage.component.jsx';
 import HomePage from './Components/homepage/homepage.component.jsx';
+import ShareImages from './Components/shareimages/shareimages.component.jsx';
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Route exact path='/' component={HomePage} />
             <Route exact path='/login' component={LoginPage} />
             <Route exact path='/register' component={RegisterPage} />
+            <Route exact path='/shareimages' component={ShareImages} />
         </Switch>
     );
 }
