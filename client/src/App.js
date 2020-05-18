dc9adc32b061cd5a4323f1c8b28444fe09bf3031
noeka <math570k@edu.ucl.dk>
2020-05-18 17:29:43 +0200

    Add some linting and render the friendlist on homepage.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css';
-import LoginPage from './Components/loginpage/loginpage.component.jsx';
-import RegisterPage from './Components/registerpage/registerpage.component.jsx';
-import HomePage from './Components/homepage/homepage.component.jsx';
-import ShareImages from './Components/shareimages/shareimages.component.jsx';
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import LoginPage from "./Components/loginpage/loginpage.component.jsx";
+import RegisterPage from "./Components/registerpage/registerpage.component.jsx";
+import HomePage from "./Components/homepage/homepage.component.jsx";
+import ShareImages from "./Components/shareimages/shareimages.component.jsx";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-    return (
-        <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route exact path='/login' component={LoginPage} />
-            <Route exact path='/register' component={RegisterPage} />
-            <Route exact path='/shareimages' component={ShareImages} />
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/register" component={RegisterPage} />
+      <Route exact path="/shareimages" component={ShareImages} />
+    </Switch>
+  );
 }
 
 export default App;
