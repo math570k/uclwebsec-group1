c8b156ca63fefac8bbe5842b495473f837575b78
Erol Bakic <erol.bakic@hotmail.com>
2020-05-20 18:03:42 +0200

    • Som bruger af systemet, skal det være muligt at kommentere de billeder som andre deler med mig.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LoginPage from './Components/loginpage/loginpage.component.jsx';
 import RegisterPage from './Components/registerpage/registerpage.component.jsx';
 import HomePage from './Components/homepage/homepage.component.jsx';
+import CommentImages from './Components/commentimages/commentimages.component.jsx';
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Route exact path='/' component={HomePage} />
             <Route exact path='/login' component={LoginPage} />
             <Route exact path='/register' component={RegisterPage} />
+            <Route exact path='/commentimages' component={CommentImages} />
         </Switch>
     );
 }
