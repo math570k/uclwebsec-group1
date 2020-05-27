26b7e259c7a658c67740950b60c230cee9eb0e8c
noeka <math570k@edu.ucl.dk>
2020-05-27 08:47:35 +0200

    Fix docker connections

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'websec',
+  host: process.env.MYSQL_HOST_IP,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = db;
